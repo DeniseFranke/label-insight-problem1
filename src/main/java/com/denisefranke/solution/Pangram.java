f36234c468a08be968fc209a15e160ea1e287877
Denise Franke <denise@denisefranke.com>
2018-01-08 18:16:49 -0600

    Add edit config to Problem 1 and update source formatting

--- src/main/java/com/denisefranke/solution/Pangram.java
+++ src/main/java/com/denisefranke/solution/Pangram.java
@@ -2,36 +2,46 @@ package com.denisefranke.solution;
 
 import java.util.Scanner;
 
+/**
+ * Pangram
+ * @author Denise Franke
+ *
+ */
 public class Pangram {
- 
-  private final static String ALPHABET =  "abcdefghijklmnopqrstuvwxyz";
- 
-  public String getMissingLetters(String sentence) {
-	StringBuilder missingLetters = new StringBuilder();
-	
-	sentence = sentence.replaceAll("[^a-zA-Z]", "").toLowerCase();
-	
-	// Save time if sentence is empty
-	if (sentence.isEmpty()) 
-		return ALPHABET;
-	
-	for (char c = 'a'; c <= 'z'; c++) {
-        if ((sentence.toLowerCase().indexOf(c) < 0) && (sentence.toLowerCase().indexOf((char)(c + 32)) < 0))
-            missingLetters.append(c);
-    }
 
-    return missingLetters.toString();
-  }
-  
-  /**
-   * //No requirement for stdin/stdout leaving it in only hurts code coverage %
-   * public static void main(String args[] ) throws Exception {   
-   * Scanner scan = new Scanner(System.in);
-   * String line = scan.nextLine();
-   * scan.close();
-   * System.out.println( new Pangram().getMissingLetters(line)  );
-  }**/
-  
- 
+	private final static String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+	/**
+	 * @param sentence
+	 * @return
+	 */
+	public String getMissingLetters(String sentence) {
+		StringBuilder missingLetters = new StringBuilder();
+
+		sentence = sentence.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
+		// Save time if sentence is empty
+		if (sentence.isEmpty())
+			return ALPHABET;
+
+		for (char c = 'a'; c <= 'z'; c++) {
+			if ((sentence.toLowerCase().indexOf(c) < 0) && (sentence.toLowerCase().indexOf((char) (c + 32)) < 0))
+				missingLetters.append(c);
+		}
+
+		return missingLetters.toString();
+	}
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String args[]) throws Exception {
+		Scanner scan = new Scanner(System.in);
+		String line = scan.nextLine();
+		scan.close();
+		System.out.println(new Pangram().getMissingLetters(line));
+	}
+
 }
  
--- src/test/java/com/denisefranke/solution/PangramTest.java
+++ src/test/java/com/denisefranke/solution/PangramTest.java
@@ -1,111 +1,100 @@
 package com.denisefranke.solution;
- 
+
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
- 
-public class PangramTest {
-  private Pangram pangram;
-  private final static String ALPHABET =  "abcdefghijklmnopqrstuvwxyz";
- 
-  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
-  @BeforeEach
-  void init() {
-	  pangram = new Pangram();
-  }
-    
-  @AfterEach
-  void tearDown() {
-    pangram = null;
-  }
- 
-  /**
-   * Test 0)  "A quick brown fox jumps over the lazy dog"
-   * 		  Returns: "" 
-   *          Reason:  This sentence contains every letter at least once
-   */
-  @DisplayName("TestPangramTest0 - empty")
-  @Test
-  void testPangramTest0() {
-    String sentence = "A quick brown fox jumps over the lazy dog";
-    String expected = "";
-    assertEquals(expected, pangram.getMissingLetters(sentence));
- 
-  }
-  
-  /**
-  * Test 1)  "A slow yellow fox crawls under the proactive dog"
-  * 			 Returns: "bjkmqz"
-  * 			 Reason:  Not a Pangram
-  */
-  @DisplayName("TestPangramTest1 - invalid")
-  @Test
-  void testPangramTest1() {
-    String sentence = "A slow yellow fox crawls under the proactive dog";
-    String expected = "bjkmqz";
-    assertEquals(expected, pangram.getMissingLetters(sentence)); 
-  }
-  
-  /**
-  * Test 2) "Lions, and tigers, and bears, oh my!"
-  * 			Returns: "cfjkpquvwxz"
-  * 			Reason:  Not a Pangram
-  **/
-  @DisplayName("TestPangramTest2 - invalid")
-  @Test
-  void testPangramTest2() {
-    String sentence = "Lions, and tigers, and bears, oh my!";
-    String expected = "cfjkpquvwxz";
-    assertEquals(expected, pangram.getMissingLetters(sentence)); 
-  }
-  
-  /**
-  * Test 3) ""
-  * 			Returns: "abcdefghijklmnopqrstuvwxyz"
-  *			Reason:  Invalid because sentence is empty;
-  **/
-  @DisplayName("Test Pangram Test3 - valid")
-  @Test
-  void testPangramTest3() {
-    String sentence = "";
-    String expected = ALPHABET;
-    assertEquals(expected, pangram.getMissingLetters(sentence)); 
-  }
-  
-  /**
-   * Test 4) Test Crazy large string
-   * 		 Returns ""
-   * 
-   */
-  @DisplayName("Test Pangram Test4 - invalid test the up to 50 characters assumptions ")
-  @Test
-  void testPangramTest4() {
-    String sentence = "CSeBBZvYvDyayyBzdfdXvaBtxxwiXcD jQADzTCXzBxzwcyxbd";
-    String expected = "ghklmnopru";
-    assertEquals(expected, pangram.getMissingLetters(sentence)); 
-  }
-  
-  
-  /**
-   * Test 5) Special Characters in sentence should be ignored
-   * 		 Returns ""
-   * 
-   */
-  @DisplayName("Test Pangram Test4 - invalid test the up to 50 characters assumptions ")
-  @Test
-  void testPangramTest5() {
-	  String sentence = "Lions, and tigers, and bears, oh my! -58 ";
-	  String expected = "cfjkpquvwxz";
-    assertEquals(expected, pangram.getMissingLetters(sentence)); 
-  }
+/**
+ * PangramTest
+ * @author Denise Franke
+ *
+ */
+public class PangramTest {
+	private Pangram pangram;
+	private final static String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+
+	@BeforeEach
+	void init() {
+		pangram = new Pangram();
+	}
+
+	@AfterEach
+	void tearDown() {
+		pangram = null;
+	}
+
+	/**
+	 * Test 0: "A quick brown fox jumps over the lazy dog" Returns: "" Reason:
+	 * This sentence contains every letter at least once
+	 */
+	@DisplayName("TestPangramTest0 - empty")
+	@Test
+	void testPangramTest0() {
+		String sentence = "A quick brown fox jumps over the lazy dog";
+		String expected = "";
+		assertEquals(expected, pangram.getMissingLetters(sentence));
+
+	}
+
+	/**
+	 * Test 1:  "A slow yellow fox crawls under the proactive dog" Returns: "bjkmqz"
+	 * Reason: Not a Pangram
+	 */
+	@DisplayName("TestPangramTest1 - invalid")
+	@Test
+	void testPangramTest1() {
+		String sentence = "A slow yellow fox crawls under the proactive dog";
+		String expected = "bjkmqz";
+		assertEquals(expected, pangram.getMissingLetters(sentence));
+	}
+
+	/**
+	 * Test 2:  "Lions, and tigers, and bears, oh my!" Returns: "cfjkpquvwxz" Reason:
+	 * Not a Pangram
+	 **/
+	@DisplayName("TestPangramTest2 - invalid")
+	@Test
+	void testPangramTest2() {
+		String sentence = "Lions, and tigers, and bears, oh my!";
+		String expected = "cfjkpquvwxz";
+		assertEquals(expected, pangram.getMissingLetters(sentence));
+	}
+
+	/**
+	 * Test 3:  "" Returns: "abcdefghijklmnopqrstuvwxyz" Reason: Invalid because
+	 * sentence is empty;
+	 **/
+	@DisplayName("Test Pangram Test3 - valid")
+	@Test
+	void testPangramTest3() {
+		String sentence = "";
+		String expected = ALPHABET;
+		assertEquals(expected, pangram.getMissingLetters(sentence));
+	}
+
+	/**
+	 * Test 4: Test Crazy large string Returns ""
+	 */
+	@DisplayName("Test Pangram Test4 - invalid test the up to 50 characters assumptions ")
+	@Test
+	void testPangramTest4() {
+		String sentence = "CSeBBZvYvDyayyBzdfdXvaBtxxwiXcD jQADzTCXzBxzwcyxbd";
+		String expected = "ghklmnopru";
+		assertEquals(expected, pangram.getMissingLetters(sentence));
+	}
+
+	/**
+	 * Test 5: Special Characters in sentence should be ignored Returns ""
+	 */
+	@DisplayName("Test Pangram Test4 - invalid test the up to 50 characters assumptions ")
+	@Test
+	void testPangramTest5() {
+		String sentence = "Lions, and tigers, and bears, oh my! -58 ";
+		String expected = "cfjkpquvwxz";
+		assertEquals(expected, pangram.getMissingLetters(sentence));
+	}
 
 }
